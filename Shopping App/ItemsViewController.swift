4f4caf435f31056eecfe623c535cb32e5139bc27
vithu <jeganathan.vithusan@my.sliit.lk>
2019-09-09 01:13:39 +0530

    Create Login Backend

---
+++ Shopping App/ItemsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ItemsViewController.swift
+//  Shopping App
+//
+//  Created by Vithusan Vithu on 9/9/19.
+//  Copyright © 2019 Nandun Bandara. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UIViewController {
+    
+    var model : Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(model)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
